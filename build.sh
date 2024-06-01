c964b06286c7bb572c7f08a29e428f1132d5e72c
StardustMotion <44144837+StardustMotion@users.noreply.github.com>
2024-06-01 20:22:55 +0900

    teleport in intro/indian opera soap transition backup

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 -file "../../mods/8bdm-classic/workshop/8C-1CUT.wad" \
 -file "../../mods/8bdm-classic/dist/8bdm-classic-1.0.0.pk3"  \
 -file "../../mods/8bdm-mbm-1.0.0.pk3"  \
-+map 8C-1CUT
++map MM1CUT
