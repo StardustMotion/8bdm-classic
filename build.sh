fa72794e03e8b53c4fc20d3e91401e603f4a7a5d
StardustMotion <44144837+StardustMotion@users.noreply.github.com>
2024-06-20 20:10:00 +0900

    cutman textures

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 ../../zandro32/zandronum.exe \
 -iwad "../../megagame.wad"  \
 -file "../../MM8BDM-v6b.pk3"  \
--file "../../mods/8bdm-classic/workshop/8C-1CUT.wad" \
 -file "../../mods/8bdm-classic/dist/8bdm-classic-1.0.0.pk3"  \
 -file "../../mods/8bdm-mbm-1.0.0.pk3"  \
 +map 8C-1CUT
